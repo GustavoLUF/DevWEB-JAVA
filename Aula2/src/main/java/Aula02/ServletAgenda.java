80491be9c42e73a2206d639a18ddacadd42a78b4
Admin <Admin@DESKTOP-80AHMFA>
2021-08-20 23:20:08 -0300

    Exercício 2:
    Meu Nome e Agenda

---
+++ Aula2/src/main/java/Aula02/ServletAgenda.java
@@ -0,0 +1,40 @@
+package Aula02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAgenda
+ */
+@WebServlet("/ServletAgenda")
+public class ServletAgenda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAgenda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+PrintWriter saida = response.getWriter();
+		
+		saida.write("<HTML><BODY>Nome: Gustavo<br>Telefone: 99999999<br>Data de Nascimento:29/12/2001</BODY></HTML>");
+		
+		saida.close();
+	}
+
+
+
+}
---
+++ Aula2/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Aula de Quinta-Feira</title>
+</head>
+<body>
+	<a href="ServletQualMeuNome">Qual o meu nome?</a><br><br>
+	<a href="ServletAgenda">Agenda do Usuário:</a>
+</body>
+</html>
